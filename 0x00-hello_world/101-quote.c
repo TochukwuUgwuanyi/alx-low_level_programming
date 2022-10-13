484c565506ecd54c1d9725859171173a056f7273
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-13 11:00:34 -0700

    wrote printf code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print out a string using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	a = ";
+	printf("and that piece of art is useful %c - Dora Korpar, 2015-10-19\n",a);
+	return (0);
+}
