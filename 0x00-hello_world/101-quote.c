e057586effb20e36f6656543f51921735013ac40
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-13 11:04:33 -0700

    wrote printf code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char a;
-	a = ";
-	printf("and that piece of art is useful %c - Dora Korpar, 2015-10-19\n",a);
+	printf("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n",a);
 	return (0);
 }
