d1bc05c0c6adc350f1919b14eea33c28c30893a1
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-15 00:35:55 -0700

    wrote a code that evalates positive, negative and zero value

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * Description: main - print out postive or negative value
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if( n < 0 ) {
+		
+		printf("n is positive\n" );
+	
+	} else if( n == 0) {
+
+		printf("n is zero\n" );
+	
+	} else
+
+	{
+		printf("n is negative\n")
+	}
+	printf("value of n is : %d\n", n);
+	
+	return (0);
+}
