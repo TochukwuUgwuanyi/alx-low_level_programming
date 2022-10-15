4d5cd5c57ccad0044d7d346bc30b5c6aa7322094
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-15 00:50:33 -0700

    wrote a code that evalates positive, negative and zero value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,14 +10,14 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+	/* my code */
 	if( n < 0 ) {
 		
-		printf("n is positive\n" );
+		printf("n is positive\n");
 	
 	} else if( n == 0) {
 
-		printf("n is zero\n" );
+		printf("n is zero\n");
 	
 	} else
 
