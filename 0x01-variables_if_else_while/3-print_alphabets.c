931641e118f01c126f8c8baefff75aa772bd6850
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-15 08:02:03 -0700

    a program that prints the alphabet in lowercase and uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * Description: main - print out lowercase/uppercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar (a);
+		putchar ('\n');
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+		putchar ('\n');
+
+	return (0);
+}
