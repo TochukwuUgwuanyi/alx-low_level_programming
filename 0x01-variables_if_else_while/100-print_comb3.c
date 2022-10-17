f9b04e677d1bb0d263559c8af3556cc0cccab399
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-17 02:24:32 -0700

    a program that prints all possible combinations of 2-digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,18 +6,18 @@
  */
 int main(void)
 {
-        int a, b;
-
-        for (a = '0'; a <= '9'; a++)
-        {
-                for (b = a + 1; b <= '9'; b++)
-                        if (a != b)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                putchar(',');
-                                putchar(' ');
-                        }
+	int a, b;
+	
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+				putchar(' ');
+			}
         }
         putchar ('\n');
         return (0);
