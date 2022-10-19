c261cbf3229c333a7506eb57b9399f0d5dbc6ea2
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-19 08:01:26 -0700

    program tests if positive_or_negative() gives the correct output when given a case of 0.

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,27 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
 #include "main.h"
+
 /**
- * Description: main - print out postive or negative value
- *
- * Return: Always 0 (Success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 void positive_or_negative(int i)
 {
-	int i;
+        int i;
 
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	} else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	} else
-	{
-		printf("%d is negative\n", i);
-	}
-	return (0);
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
