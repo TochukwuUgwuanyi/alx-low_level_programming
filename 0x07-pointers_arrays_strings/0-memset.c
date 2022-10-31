4385f6198a08be2e4f33bdf9752463d8cd2f86d8
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-10-31 13:44:20 -0700

    a function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_memset -used to fill a block of memory with a particular value
- * @*s:  parameter for starting address of memory to be filled
+ * *_memset -used to fill a memory with a particular value
+ * @*s:  parameter for address of memory to be filled
  * @b: parameter to be filled
  * @n: maximum number bytes usable
  * Return: Always 0.
