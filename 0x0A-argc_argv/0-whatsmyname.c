1e01550fa59fa6032ddc4e5ac920ade3e825b159
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-11-04 23:53:29 -0700

    a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -12,8 +12,7 @@ int main(int argc, char *argv[])
 	int i;
 	for(i = 0; i < argc; i++)
 	{
-		(void)argc;
-		printf("program's name is: %s\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return 0;
 }
