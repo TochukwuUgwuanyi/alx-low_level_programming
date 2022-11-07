c2b0cb14b949b5e240bf4190f711183162d07862
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2022-11-07 11:28:17 -0800

    a program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 /**
  * main -used to fill memory with particular value
  * @argc:  contains total number of argument/parameter
  * @*argv: array of strings
- * Return: 0
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
