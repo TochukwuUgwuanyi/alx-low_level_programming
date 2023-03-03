b257ea46ddeb05f16a5614f66bee6ad60998ac73
TochukwuUgwuanyi <tochukwuugwuanyi@gmail.com>
2023-03-03 03:57:23 -0800

    updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,11 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 /**
- * File: main
+ * File: main code
  *
- * Desc: header file
+ * name: name
+ *
+ * Description: header file
 **/
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
